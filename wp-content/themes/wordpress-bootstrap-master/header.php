cb90304fa70c029efba2bf7b5a3ad1dbe57a5129
Jovan Marjanovic <jovan.marjanovic@pauk-net.com>
2014-01-27 05:22:26 -0800

    postavljanje projekta

---
+++ wp-content/themes/wordpress-bootstrap-master/header.php
@@ -0,0 +1,94 @@
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+				
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				
+		<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+
+		<!-- theme options from options panel -->
+		<?php /*get_wpbs_theme_options(); */?>
+
+		<!-- typeahead plugin - if top nav search bar enabled -->
+		<?php require_once('library/typeahead.php'); ?>
+				
+	</head>
+	
+	<body <?php body_class(); ?>>
+				
+		<header role="banner" class="header">
+		
+			<div id="inner-header" class="clearfix">
+				
+				<div class="navbar">
+					<div class="navbar-inner">
+						<div class="container-fluid nav-container">
+							<nav role="navigation">
+								<a class="brand" id="logo" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>">
+									<?php if(of_get_option('branding_logo','')!='') { ?>
+										<img src="<?php echo of_get_option('branding_logo'); ?>" alt="<?php echo get_bloginfo('description'); ?>">
+										<?php }
+										if(of_get_option('site_name','1')) bloginfo('name'); ?></a>
+								
+								<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+								</a>
+								
+								<div class="nav-collapse">
+									<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+								</div>
+								
+							</nav>
+							
+							<?php /*if(of_get_option('search_bar', '1')) {?>
+							<form class="navbar-search pull-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+								<input name="s" id="s" type="text" class="search-query" autocomplete="off" placeholder="<?php _e('Search','bonestheme'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
+							</form>
+							<?php } */?>
+							
+						</div> <!-- end .nav-container -->
+					</div> <!-- end .navbar-inner -->
+				</div> <!-- end .navbar -->
+			
+			</div> <!-- end #inner-header -->
+
+			<div class="row-fluid">
+				<div class="span12">
+					<div class="container" style="margin-top: -55px;">
+						<ul id="carousel" style="height: 310px; margin: 0;">
+							<li><img src="/wp-content/themes/wordpress-bootstrap-master/library/images/slide1.png" alt="" /></li>
+							<li><img src="/wp-content/themes/wordpress-bootstrap-master/library/images/slide3.png" alt="" /></li>
+						</ul>
+					</div>
+				</div><!-- /span -->
+			</div><!-- /row -->
+		
+		</header> <!-- end header -->
+		
+		<div class="container-fluid wrapper">
